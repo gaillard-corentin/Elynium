8bd2fae1bee50fd5654c0f05f3a5fb34b398279c
swannbrunet <swannbrunet@hotmail.fr>
2016-02-24 15:23:16 +0100

    mise à jour site
    mise à jour

--- site internet/QSN.php
+++ site internet/QSN.php
@@ -13,11 +13,45 @@
 	     <h1>Qui somme nous ?</h1>
 		    <h2>historique</h2>
 		        <p>
-					inserer la presentation du groupe qu'il y a dans le cahier des charges
+					Pour effectuer ce projet, nous avons formé un groupe de quatre étudiants de première année de préparatoire à l'EPITA.
+					La première difficulté rencontrée a été lors de la création du groupe. 
+					En effet, une composition de quatre membres par équipe était obligatoire. 					
 		        </p>
+				<p>
+					Nous avons cependant eu pour force de choisir un thème à l'unanimité.
+					Ce qui nous fait penser que tous les membre du groupe vont être impliqués dans son avancement.
+					De plus ayant déjà eu l'occasion de travailler ensemble, nous connaissons la façon de travailler de chacun.
+				</p>
 				<hr width="100%" color="#848484">
 		    <h2>L'équipe</h2>
-		        <p>inserer la presentation des membres</p>
+		        <p>
+					PHELISSE Brice <Br/>
+					Je suis le fondateur du groupe, étant passionné d'histoire, de STR et de RPG, j'ai proposé le str pour ses nombreux points et atouts en programmation.
+					Ceci n'est pas mon premier gros projet en programmation, je suis déjà passé par trois autres projets nécessitant la gestion de petite équipe.
+					Le jeux vidéo m'a amené comme beaucoup dans le monde de l'informatique, mais rapidement, ma passion pour l'informatique s'étendra au delà du loisir video-ludique.
+				</p>
+				<p>
+					BRUNET Swann <Br/>
+					Je suis arrivée en dernier sur le groupe déjà composé de trois personnes. 
+					Par conséquent le thème du projet était déjà défini mais ce qui ne ma pas empêché de l’adopter tout de suite. 
+					Cela fait depuis maintenant bien longtemps que je m’intéresse au secteur de l’informatique et de la programmation.  
+					Je n’ai jamais eu l’opportunité de pouvoir réaliser un travail de groupe aussi conséquent. 
+					Ainsi j’obtiens une certaine appréhension quand au développement mais je reste tout de même assez confient. 
+					Ce projet m’apportera de nouvelle compétence sur de nouveaux domaines.
+					Je pense, avec le peu de pratique que j’ai pour le moment, que ce projet de groupe va me permettre de explorer une nouvelle dynamique de fonctionnement et de travail.
+					Je vais avec ce projet découvrir de nouveau outil tourné sur le divertissement mais aussi sur la création à l’aboutissement d’un projet.
+				</p>
+				<p>
+					GAILARD Corentin <Br/>
+					Depuis longtemps, je suis un joueur de jeux de stratégie en temps réel et souvent dans une époque médiéval comme Stronghold, j'ai voulu reprendre ce style de jeux auquel je joue depuis longtemps.
+					De plus, je suis très intéressé par l'expérience que m'apportera le projet de sup, qui sera mon premier 'vrai' projet. 
+					En espérant apprendre de nouvelles méthodes de travaille ainsi que la découverte de nouveaux logiciels.
+				</p>
+				<p>
+					ROUDAUT François-Joseph <Br/>
+					Depuis presque toujours passionné de jeux de stratégie en temps réel, ce projet va me permettre de redécouvrir le genre en me mettant cette fois à la place du programmeur. 
+					n'ayant jamais programmé avant cette première année à l'EPITA (à l'exception du TIbasic de ma calculatrice au lycée) j'espère apprendre beaucoup de ce projet et mettre en pratique mes connaissances en programmation.
+				</p>
 				<hr width="100%" color="#848484">
 		    <h2>organigramme</h2>
 			<ul>
