f787be3a9016084b1342a1dadbc7f3a91e466400
FranNjo <francois-joseph.roudaut@epita.fr>
2016-03-05 19:30:56 +0100

    MModification du Menu Signed-off-by: FranNjo <francois-joseph.roudaut@epita.fr>

---
+++ Elynium/Assets/Menu/Scripts/Charger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charger : MonoBehaviour {
+
+	public void Charger_scene()
+    {
+        //Application.LoadLevel(0);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Elynium/Assets/Menu/Scripts/Charger_Map.js
@@ -0,0 +1,23 @@
+﻿var texte;
+texte = GetComponentInChildren(MeshRenderer);
+var Map_to_load;
+
+function OnMouseEnter() {
+    texte.material.color = Color.red;
+}
+
+function OnMouseExit() {
+    texte.material.color = Color.white;
+}
+
+function OnMouseUp() {
+    Application.LoadLevel(Map_to_load);
+}
+
+function OnMouseDown() {
+    texte.material.color = Color.green;
+}
+
+function SelectMap_Les_plages_dElynium() {
+    Map_to_load = "ElyniumTestMap";
+}
